32a2614743b051dae49fb63cc49b044fbb92e9fa
PITRv1 <petersandor.toth@gmail.com>
2025-03-16 20:14:43 +0100

    disable camera movement in maingamescene

--- Assets/Scipts/CameraSystem.cs
+++ Assets/Scipts/CameraSystem.cs
@@ -17,21 +17,21 @@ public class CameraSystem : MonoBehaviour {
         cameraInputActions.Camera.Enable();
     }
 
-    private void Update() {
+    //private void Update() {
 
-        float rotateDirY = cameraInputActions.Camera.Rotation.ReadValue<Vector2>().x;
-        float rotateDirX = cameraInputActions.Camera.Rotation.ReadValue<Vector2>().y;
+    //    float rotateDirY = cameraInputActions.Camera.Rotation.ReadValue<Vector2>().x;
+    //    float rotateDirX = cameraInputActions.Camera.Rotation.ReadValue<Vector2>().y;
 
-        float rotateSpeed = 100f;
+    //    float rotateSpeed = 100f;
 
-        transform.eulerAngles += new Vector3(0, -rotateDirY * rotateSpeed * Time.deltaTime, 0);
+    //    transform.eulerAngles += new Vector3(0, -rotateDirY * rotateSpeed * Time.deltaTime, 0);
 
-        Vector3 moveDir = cameraInputActions.Camera.Movement.ReadValue<Vector3>();
+    //    Vector3 moveDir = cameraInputActions.Camera.Movement.ReadValue<Vector3>();
 
-        float moveSpeed = 50f;
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
+    //    float moveSpeed = 50f;
+    //    transform.position += moveDir * moveSpeed * Time.deltaTime;
 
-    }
+    //}
 
     public void DisableInput() {
         cameraInputActions.Disable();
